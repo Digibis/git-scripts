3d67cde01cb47182b94c45a4cffa5a69cdd5d8bc
Luis Panadero Guardeño <luis.panadero@digibis.com>
2025-07-18 08:45:16 +0200

    Fetch.sh script now avoids trying to run git fetch on a folder that isn't a git respository or run multiple times over a git reposity that have worktress on the same base folder

--- fetch.sh
+++ fetch.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+echo $(pwd)
 echo "This script does fetch --prune on all repositories"
 
 fetch() {
   local dir=$1
   pushd "$dir" > /dev/null
-  echo "Fetch on $dir"
-  git fetch --prune
+  if [ -d "./.git" ] ; then
+    echo "Fetch on $dir"
+    git fetch --prune
+  fi
   popd > /dev/null
 }
 
 shopt -s dotglob
-find * -prune -type d | while IFS= read -r dir; do 
+find * -prune -type d | while IFS= read -r dir; do
   fetch $dir
 done
 
